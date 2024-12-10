bb7e6d05905145892acecbb266658f727f675347
MiningExpert99 <gnickolas67@gmail.com>
2024-12-10 13:37:24 -0500

    2.4 DONE

--- BasicGameplay/Assets/Scripts/SpawnManager.cs
+++ BasicGameplay/Assets/Scripts/SpawnManager.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+   
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float startDelay = 2;
+    private float SpawnInterval = 1;
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, SpawnInterval);
     }
 
     // Update is called once per frame
@@ -15,13 +19,16 @@ public class SpawnManager : MonoBehaviour
     private float spawnPosZ = 20;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+      
+
             // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
-                animalPrefabs[animalIndex].transform.rotation);
+
+            void SpawnRandomAnimall()
+            {
+                int animalIndex = Random.Range(0, animalPrefabs.Length); ;
+                Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+                Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+                    animalPrefabs[animalIndex].transform.rotation);
+            }
         }
     }
-}
